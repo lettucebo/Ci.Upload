070824e8673f6dd1553b6b4b81dfe1c8b3483c76
紙鈔魚 <abc12207@gmail.com>
2015-11-09 19:10:26 +0800

    add some class

---
+++ Creatidea.Library.Uploads/Imgur/CiImgurHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Creatidea.Library.Uploads.Imgur
+{
+    using System.Web;
+
+    /// <summary>
+    /// Imgur 擴充方法.
+    /// </summary>
+    public static class CiImgurHelper
+    {
+        /// <summary>
+        /// Save to the imgur.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns>System.String.</returns>
+        public static string ToImgur(this HttpPostedFileBase file)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Save to the imgur with token.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <param name="token">The token.</param>
+        /// <returns>System.String.</returns>
+        public static string ToImgur(this HttpPostedFileBase file, string token)
+        {
+            return null;
+        }
+    }
+}
