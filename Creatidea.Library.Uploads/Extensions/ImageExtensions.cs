a70981df4d9ca97247b23476100b0ef2da7b6fa6
紙鈔魚 <lettucebo@hotmail.com>
2016-08-04 08:49:41 +0800

    feat: 加入image存檔功能

---
+++ Creatidea.Library.Uploads/Extensions/ImageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creatidea.Library.Uploads.Extensions
+{
+    using System.Drawing;
+
+    using Creatidea.Library.Uploads.Models;
+    using Creatidea.Library.Uploads.Service;
+
+    public static class ImageExtensions
+    {
+        public static CiImageFile SaveAsLocal(this Image image, string folder = "Temps")
+        {
+            return CiFileService.SaveLocal(image, image.RawFormat, folder);
+        }
+    }
+}
--- Creatidea.Library.Uploads/Service/CiFileService.cs
+++ Creatidea.Library.Uploads/Service/CiFileService.cs
@@ -11,6 +11,8 @@ namespace Creatidea.Library.Uploads.Service
     using System.IO;
     using System.Web;
 
+    using Ci.Extensions;
+
     using Creatidea.Library.Configs;
     using Creatidea.Library.Uploads.Models;
 
@@ -52,7 +54,7 @@ namespace Creatidea.Library.Uploads.Service
         /// <param name="folder">欲存放資料夾名稱</param>
         /// <returns>T.</returns>
         /// <exception cref="System.NullReferenceException">檔案為空或是沒有任何檔案長度</exception>
-        protected internal static CiFile SaveLocal(HttpPostedFileBase file, string folder)
+        protected internal static CiFile SaveLocal(HttpPostedFileBase file, string folder = "Temps")
         {
             if (IsNullOrEmpty(file))
             {
@@ -88,6 +90,53 @@ namespace Creatidea.Library.Uploads.Service
 
             // 存檔
             file.SaveAs(model.FullPath);
+
+            model.VirtualPath = Path.Combine(model.VirtualPath, model.NewName + model.Extension);
+
+            return model;
+        }
+
+        /// <summary>
+        /// 圖片儲存
+        /// </summary>
+        /// <param name="image">The image.</param>
+        /// <param name="imageFormat">The image format.</param>
+        /// <param name="folder">欲存放資料夾名稱</param>
+        /// <returns>CiResult FileUploadViewModel</returns>
+        protected internal static CiImageFile SaveLocal(Image image, ImageFormat imageFormat, string folder = "Temps")
+        {
+            if (image == null)
+            {
+                throw new NullReferenceException("圖片為空！");
+            }
+
+            CiImageFile model = new CiImageFile()
+            {
+                Extension = imageFormat.ToFileExtension(),
+                NewName = GetNewFileName(),
+                Folder = folder,
+                VirtualPath = Path.Combine(RootPath, folder),
+                Format = imageFormat
+            };
+            model.FullPath =
+                HttpContext.Current.Server.MapPath(Path.Combine(model.VirtualPath, model.NewName + model.Extension));
+
+            // 若檔名重複則自動重新取得編號
+            while (File.Exists(model.FullPath))
+            {
+                model.NewName = GetNewFileName();
+                model.FullPath =
+                    HttpContext.Current.Server.MapPath(Path.Combine(model.VirtualPath, model.NewName + model.Extension));
+            }
+
+            // 檢查資料夾是否存在，若不存在則自動建立
+            if (!Directory.Exists(Path.GetDirectoryName(model.FullPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(model.FullPath));
+            }
+
+            // 存檔
+            image.Save(model.FullPath, imageFormat);
 
             model.VirtualPath = Path.Combine(model.VirtualPath, model.NewName + model.Extension);
 
