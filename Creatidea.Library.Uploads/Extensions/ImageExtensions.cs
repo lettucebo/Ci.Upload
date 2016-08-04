79825d1d8bf3e7034294fa00e182b99ac9b37f83
紙鈔魚 <lettucebo@hotmail.com>
2016-08-04 08:56:18 +0800

    feat: 圖片存檔加入可從呼叫指定圖片格式

--- Creatidea.Library.Uploads/Extensions/ImageExtensions.cs
+++ Creatidea.Library.Uploads/Extensions/ImageExtensions.cs
@@ -7,15 +7,18 @@ using System.Threading.Tasks;
 namespace Creatidea.Library.Uploads.Extensions
 {
     using System.Drawing;
+    using System.Drawing.Imaging;
 
     using Creatidea.Library.Uploads.Models;
     using Creatidea.Library.Uploads.Service;
 
     public static class ImageExtensions
     {
-        public static CiImageFile SaveAsLocal(this Image image, string folder = "Temps")
+        public static CiImageFile SaveAsLocal(this Image image, ImageFormat format = null, string folder = "Temps")
         {
-            return CiFileService.SaveLocal(image, image.RawFormat, folder);
+            if (format == null)
+                format = image.RawFormat;
+            return CiFileService.SaveLocal(image, format, folder);
         }
     }
 }
